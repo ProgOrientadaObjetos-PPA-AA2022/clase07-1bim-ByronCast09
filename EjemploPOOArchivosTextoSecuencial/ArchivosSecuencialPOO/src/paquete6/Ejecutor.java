152fcaa13a4bc81f72578eaf2bb18bfd6d19baa0
Kevin0232 <kevinbust0232@gmail.com>
2022-05-26 23:53:04 -0500

    Clase07Resuelto

--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Ejecutor.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Ejecutor.java
@@ -10,5 +10,29 @@ package paquete6;
  * @author reroes
  */
 public class Ejecutor {
-    
+
+    public static void main(String[] args) {
+         // nombre del archivo
+        String nombreArchivo = "Hospital.txt";
+        
+        Hospital h1 = new Hospital("San Isidro",260,2643.2);
+        Hospital h2 = new Hospital("UTPL",452,986.3);
+        Hospital h3 = new Hospital("Cruz Roja",360,5662.2);
+        
+        ArchivoEscritura archivo = new ArchivoEscritura(nombreArchivo);
+        
+        Hospital[] lista = {h1, h2, h3};
+        
+        for (int i = 0; i < lista.length; i++) {
+            // establecer el valor del atributo registro
+            archivo.establecerRegistro(lista[i]);
+            // establecer en el archivo el atributo del registro
+            archivo.establecerSalida();
+        }
+        
+        ArchivoLectura lectura = new ArchivoLectura(nombreArchivo);
+        lectura.establecerLista();
+        System.out.println(lectura);
+        
+    }
 }
--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Hospital.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Hospital.java
@@ -13,6 +13,38 @@ public class Hospital {
     String nombre;
     int numeroCamas;
     double presupuesto;
+    
+    public Hospital(String n, int numC,double pre){
+        nombre = n;
+        numeroCamas = numC;
+        presupuesto = pre;
+        
+    }
+
+    
+    public void establecerNombre(String n){
+        nombre = n;
+    }
+    
+    public void establecerNumeroCamas(int numC){
+        numeroCamas = numC;
+    }
+    
+    public void establecerPresupuesto(int pre){
+        numeroCamas = pre;
+    }
+    
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public int obtenerNumeroCamas(){
+        return numeroCamas;
+    }
+    
+    public double obtenerPresupuesto(){
+        return presupuesto;
+    }
 }
 
 /* 
